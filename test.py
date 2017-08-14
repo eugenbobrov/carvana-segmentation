3d792c6a921efcb561b7be66f9201054c48960a5
eugenbobrov <eugenbobrov@ya.ru>
2017-08-14 15:06:02 +0300

    hm

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-path = TRAIN_DIR + id_list[0].split('_')[0] + '_'
-path =  [path + str(j + 1).zfill(2) + '.jpg' for j in range(16)]
-data = imread_collection(path).concatenate().mean(3)
-data -= data.mean(0)
-
-
-
